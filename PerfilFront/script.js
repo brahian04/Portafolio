70bea3fd1bb25d52f2f3a3198976fbb9bb015e50
Brahian Díaz <brahiandll4@gmail.com>
2023-01-27 12:24:22 -0600

    secciones completas y responsive design

---
+++ PerfilFront/script.js
@@ -0,0 +1,41 @@
+// funcion que aplica estilo a la opción seleccionada y quita la previamente seleccionada
+function seleccionar(link){
+    var opciones= document.querySelectorAll('#links a');
+    opciones[0].className= "";
+    opciones[1].className= "";
+    opciones[2].className= "";
+    opciones[3].className= "";
+    opciones[4].className= "";
+    link.className= "seleccionado";
+
+    // se hace desaparecer el menu cuando se ha seleccionado una opción en el modo reponsive
+    var x= document.getElementById("nav");
+    x.className= "";
+}
+
+// Menú responsive
+function responsiveMenu(){
+    var x= document.getElementById("nav");
+    if(x.className===""){
+        x.className= "responsive";
+    }else{
+        x.className= "";
+    }
+}
+
+// //detectar scrolling para iniciar la animación de las barras de habilidades
+// window.onscroll= function(){
+//     efectoHabilidades()
+// };
+
+// // función que aplica la animación de la barra
+// function efectoHabilidades(){
+//     var skills= document.getElementById("skills");
+//     var distancia_skills= window.innerHeight - skills.getBoundingClientRect().top;
+//     if(distancia_skills>=300){
+//         document.getElementById("html").classList.add("barra-progreso1");
+//         document.getElementById("javascript").classList.add("barra-progreso2");
+//         document.getElementById("bd").classList.add("barra-progreso3");
+//         document.getElementById("git").classList.add("barra-progreso4");
+//     }
+// }
